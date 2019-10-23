070197354f7b7602895ecca4085e5332b2465e9a
HurricaneInteractive <hurricane.interactive@gmail.com>
2019-10-23 21:19:19 +1100

    Basic camera rotate script

---
+++ Assets/Scripts/Camera/CameraRotate.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRotate : MonoBehaviour {
+    public GameObject terrain;
+    public float moveSpeed = 10.0f;
+    public float rotateSpeed = 10.0f;
+
+    // Update is called once per frame
+    void Update() {
+        float h = Input.GetAxis("Horizontal") * moveSpeed;
+        transform.RotateAround(terrain.transform.position, Vector3.up, rotateSpeed * (h * -1) * Time.fixedDeltaTime);
+    }
+}
