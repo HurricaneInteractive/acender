ba40a9fb9457f315eb320f5d0e47e126cc49b23a
HurricaneInteractive <hurricane.interactive@gmail.com>
2019-10-28 20:21:50 +1100

    Rotate the camera by exactly 90 degrees on key press

--- Assets/Scripts/Camera/CameraRotate.cs
+++ Assets/Scripts/Camera/CameraRotate.cs
@@ -3,13 +3,38 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraRotate : MonoBehaviour {
-    public GameObject terrain;
-    public float moveSpeed = 10.0f;
+    private bool canRotate = true;
+    private Quaternion startingRotation;
+
     public float rotateSpeed = 10.0f;
+
+    void Start() {
+        startingRotation = transform.rotation;
+    }
 
     // Update is called once per frame
     void Update() {
-        float h = Input.GetAxis("Horizontal") * moveSpeed;
-        transform.RotateAround(terrain.transform.position, Vector3.up, rotateSpeed * (h * -1) * Time.fixedDeltaTime);
+        float h = Input.GetAxisRaw("Horizontal");
+
+        if (h != 0 && canRotate) {
+            canRotate = false;
+            StopAllCoroutines();
+            StartCoroutine(Rotate(h));
+        }
+    }
+
+    IEnumerator Rotate(float direction) {
+        float rotateBy = direction > 0 ? -90 : 90;
+        Quaternion finalRotation = Quaternion.Euler(0, rotateBy, 0) * startingRotation;
+
+        while (transform.rotation != finalRotation) {
+            transform.rotation = Quaternion.Lerp(transform.rotation, finalRotation, Time.deltaTime * rotateSpeed);
+            yield return null;
+        }
+
+        transform.rotation = finalRotation;
+        startingRotation = finalRotation;
+        canRotate = true;
+        StopAllCoroutines();
     }
 }
